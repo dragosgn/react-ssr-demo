a7d2a9a9faf6f2e5f72a5f024b5bc23f72b3f432
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-02 13:42:07 +0200

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      I am being rendered on the Server.
+      <br />
+      <br />
+      So part of the hard work is done by the Server.
+      <br />
+      <br />I bring a lot of advantages to the table.
     </div>
   );
 }
--- src/server/middleware/render.js
+++ src/server/middleware/render.js
@@ -1,20 +1,20 @@
-import escapeStringRegexp from 'escape-string-regexp';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import escapeStringRegexp from "escape-string-regexp";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-import App from '../../App';
+import App from "../../App";
 
 const renderMiddleware = () => (req, res) => {
   let html = req.html;
-  const htmlContent = ReactDOMServer.renderToString(<App />);
+  const htmlContent = ReactDOMServer.renderToString(<App />); // Here is where the magic happens!
   const htmlReplacements = {
-    HTML_CONTENT: htmlContent,
+    HTML_CONTENT: htmlContent
   };
 
   Object.keys(htmlReplacements).forEach(key => {
     const value = htmlReplacements[key];
     html = html.replace(
-      new RegExp('__' + escapeStringRegexp(key) + '__', 'g'),
+      new RegExp("__" + escapeStringRegexp(key) + "__", "g"),
       value
     );
   });
